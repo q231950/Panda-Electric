53da0792782b90f0c0e6d80cf50ab2ec777f0b55
Martin Kim Dung-Pham <kim@elbedev.com>
2016-07-30 10:40:55 +0200

    Initial Commit

---
+++ Panda Electric/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Panda Electric
+//
+//  Created by Martin Kim Dung-Pham on 30/07/16.
+//  Copyright © 2016 elbedev.com. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Panda Electric/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Panda Electric
+//
+//  Created by Martin Kim Dung-Pham on 30/07/16.
+//  Copyright © 2016 elbedev.com. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
