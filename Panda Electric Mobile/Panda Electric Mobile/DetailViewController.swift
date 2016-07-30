f8fcf15ca5cb1b808ef262f8e8fa1b291d4aff09
Martin Kim Dung-Pham <kim@elbedev.com>
2016-07-30 11:29:33 +0200

    add Panda Electric Mobile

---
+++ Panda Electric Mobile/Panda Electric Mobile/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  Panda Electric Mobile
+//
+//  Created by Martin Kim Dung-Pham on 30/07/16.
+//  Copyright © 2016 elbedev.com. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    var detailItem: AnyObject? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = self.detailItem {
+            if let label = self.detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
