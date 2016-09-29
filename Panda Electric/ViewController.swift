c853fa3ffae062797e48f567335d8cd3c1b6a775
Martin Kim Dung-Pham <kim@elbedev.com>
2016-09-29 08:32:01 +0200

    Make macOS app compile and send text value

--- Panda Electric/ViewController.swift
+++ Panda Electric/ViewController.swift
@@ -10,12 +10,13 @@ import Cocoa
 import Birdsong
 import Panda
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, NSTextFieldDelegate {
 
     @IBOutlet var inputField: NSTextField!
     @IBOutlet var messageLabel: NSTextField!
-//    let socket = Socket(url: NSURL(string: "http://localhost:4000/socket/websocket")!)
-    let socket = Socket(url: NSURL(string: "https://tranquil-peak-78260.herokuapp.com/socket/websocket")!)
+    let socket = Socket(url: "http://localhost:4000/socket/websocket")
+    //    let socket = Socket(url: "https://tranquil-peak-78260.herokuapp.com/socket/websocket")
+
     var socketHandler: SocketHandler? {
         didSet {
             socketHandler?.messageHandler = { (message: String, position: Int) -> Void in
@@ -26,6 +27,8 @@ class ViewController: NSViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        inputField.delegate = self
 
         socket.onConnect = {
             print("Socket connected")
@@ -35,19 +38,28 @@ class ViewController: NSViewController {
         socket.connect()
     }
     
-    private func handleMessage(message: String, atPosition position: Int) {
+    fileprivate func handleMessage(_ message: String, atPosition position: Int) {
         if let messageLabel = self.messageLabel {
             messageLabel.stringValue = message + " \(position)"
         }
     }
 
-    override var representedObject: AnyObject? {
+    override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
         }
     }
 
-    
+    // MARK: NSTextFieldDelegate
+    override func controlTextDidEndEditing(_ obj: Notification) {
+        guard inputField.stringValue.lengthOfBytes(using: .utf8) > 0 else {
+            return
+        }
+        
+        if let socketHandler = socketHandler {
+            socketHandler.sendMessage(message: inputField.stringValue)
+        }
+    }
 
 }
 
